0ac695447089b80e47e15849f1e7a0391ca3fde4
AjaypalSingh <er.ajaypal@gmail.com>
2022-04-01 17:58:00 +1100

    init laravel app

---
+++ database/migrations/2022_03_30_060809_create_fruits_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFruitsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fruits', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('org_label', 255)->index();
+            $table->string('upd_label', 255)->index();
+            $table->unsignedInteger('parent_id')->nullable()->index();
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('fruits');
+    }
+}
---
+++ resources/views/app.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>@yield('title')</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+
+        <!-- jQuery library -->
+        <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+
+        <!-- Popper JS -->
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+
+        <!-- Latest compiled JavaScript -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+
+
+        @yield('meta')
+    </head>
+    <body class="">
+        <div class="container pt-2 pb-2">
+            @yield('content')
+        </div>
+    </body>
+    <footer>
+        @yield('footer-scripts')
+    </footer>
+</html>
+
+<style>
+    [contenteditable="true"] {
+        padding: 2px 10px;
+        border: 1px solid #999;
+        font-size: 90%;
+    }
+</style>
+
